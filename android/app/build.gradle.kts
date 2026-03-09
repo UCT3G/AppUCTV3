0132fc61731f46dffb32be014a1963e8ff3d6382
Nora Vazquez <vazquez.nora.g@gmail.com>
2026-03-09 17:15:35 -0600

    cambios gradle version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.11"
+        versionCode = 13
+        versionName = "1.12"
     }
 
     signingConfigs {
