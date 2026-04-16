f3eba05cb50a78545087282c3642a5c973492e67
Nora Vazquez <102182365+noravazquez@users.noreply.github.com>
2026-04-16 00:14:36 -0600

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,7 +4,7 @@
 class ApiService {
   // static const String baseURL = 'http://192.168.1.7:8007';
   // static const String baseURL = 'http://192.168.1.198:8007';
-  static const String baseURL = 'http://10.10.31.14:8005';
+  static const String baseURL = 'http://192.168.1.142:8007';
   // static const String baseURL = 'https://uct.tresguerras.com.mx/uct_app';
 
   // METODO PARA HACER SOLICITUDES GET
