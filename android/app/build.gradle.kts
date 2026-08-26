03a7a8c75738852cee6aaeb63c161f5d6214a0a4
Nora Vazquez <102182365+noravazquez@users.noreply.github.com>
2026-08-25 23:41:37 -0600

    Actualizacion de la app para android 16

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.appuctdemodev.app_uct"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -34,9 +34,9 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
-        targetSdk = 35
-        versionCode = 17
-        versionName = "1.16"
+        targetSdk = 36
+        versionCode = 18
+        versionName = "1.17"
     }
 
     signingConfigs {
